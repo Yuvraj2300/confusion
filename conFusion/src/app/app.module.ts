d9c8649757c30d0dc4dbf22757945f134621b889
yuvraj <yuvraj_2300@hotmail.com>
2019-05-30 23:37:48 +0530

    NG COMPONENTS NOT RENDERING

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 import 'hammerjs';
 
@@ -20,7 +24,10 @@ import { MenuComponent } from './menu/menu.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
